b4e900add976752e8c3ad4270a986a1dba97836c
Mario Sieg (KerboGames) <support@kerbogames.com>
2020-09-09 23:12:26 +0200

    Updates

--- src/chunk.rs
+++ src/chunk.rs
@@ -204,160 +204,66 @@
 
 */
 
-use super::argument::*;
-use super::asm;
+use super::record::RecordUnion;
 
-/// The category of an instruction type.
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum InstructionType {
-    /// The instruction is for VM controlling.
-    System,
-
-    /// The instruction is for memory controlling.
-    Memory,
-
-    /// The instruction is for flow and brach control.
-    Control,
-
-    /// The instruction is for arithmetic operations.
-    Arithmetic,
-
-    /// This instruction is for logical operations.
-    Logical,
-
-    /// This instruction is for bitwise operations.
-    Bitwise,
+/// Contains a continous chunk of bytecode and a program counter.
+pub struct BytecodeChunk {
+    buf: Box<[RecordUnion]>,
+    pc: usize,
 }
 
-/// Represents an instruction.
-/// Contains metadata for bytecode parsing verification.
-pub struct InstructionMeta {
-    /// The opcode of this instruction taken from the list above.
-    pub opcode: u32,
+impl BytecodeChunk {
+    #[inline]
+    pub fn with_buf(buf: Box<[RecordUnion]>) -> Self {
+        Self { buf, pc: 0 }
+    }
 
-    /// The bytecoee mnemonic of this instruction.
-    pub mnemonic: &'static str,
-
-    /// A short but precise description of what this
-    /// instruction does and how it should be used.
-    /// If there are any dangers of using this instruction,
-    /// also write them down here!
-    pub description: &'static str,
-
-    /// Stack pointer offset after execution.
-    /// Use isize::MAX to indicate unknown push count!
-    /// Use isize::MIN to indicate unknown pop count!
-    pub stack_modificator: isize,
-
-    /// Set to true if this instruction jumps or executes any
-    /// branches or flow control modifications.
-    pub is_modifying_flow_control: bool,
-
-    /// Set to true if this instruction modifies anything on the stack.
-    pub is_modifying_stack: bool,
-
-    /// Set to true if this instruction modifies anything on the heap.
-    pub is_modifying_heap: bool,
-
-    /// The instruction type category of this instruction.
-    pub instruction_type: InstructionType,
-
-    /// Contains meta data about all arguments.
-    /// Will be heavily used by the bytecode verifier.
-    pub arguments: &'static [ArgumentMeta],
+    #[inline]
+    pub fn with_buf_pc(buf: Box<[RecordUnion]>, pc: usize) -> Self {
+        Self { buf, pc }
+    }
 }
 
-impl InstructionMeta {
-    /// Contains the metadata of all instructions.
-    /// The opcodes can be used to lookup.
-    pub const META_TABLE: &'static [InstructionMeta] = &[
-        ///////////////////////////////
-        InstructionMeta {
-            opcode: asm::INTERRUPT,
-            mnemonic: "int",
-            description: r#" 
-                Interrupts execution. 
-                If 0 is passed it ends the program, else it triggers 
-                the exception with the corresponding id.
-            "#,
-            stack_modificator: 0,
-            is_modifying_flow_control: false,
-            is_modifying_stack: false,
-            is_modifying_heap: false,
-            instruction_type: InstructionType::System,
-            arguments: &[ArgumentMeta {
-                visibility_type: ArgumentVisibilityType::Explicit,
-                value_type: ArgumentValueType::Int,
-                policy: ArgumentPolicy::Required,
-                description: r#"
-                        The interrupt code. If the code is <= 0
-                        it will end the program and use it as exit code.
-                        Else an exception with the interrupt code will be triggerd.
-                        If there is not exception with that interrupt code,
-                        and unkown exeption will be raised.
-                    "#,
-            }],
-        },
-        ///////////////////////////////
-        InstructionMeta {
-            opcode: asm::PUSH,
-            mnemonic: "push",
-            description: r#"
-                Pushes the argument onto the stack.
-                The argument must be value type, no reference!
-            "#,
-            stack_modificator: 1,
-            is_modifying_flow_control: false,
-            is_modifying_stack: true,
-            is_modifying_heap: false,
-            instruction_type: InstructionType::Memory,
-            arguments: &[ArgumentMeta {
-                visibility_type: ArgumentVisibilityType::Explicit,
-                value_type: ArgumentValueType::AnyValueType,
-                policy: ArgumentPolicy::Optional(ArgumentDefaultValue::Int(0)),
-                description: r#"
-                        The value to push onto the stack.
-                        Can be any value type, which fits into a stack record.
-                    "#,
-            }],
-        },
-        ///////////////////////////////
-        InstructionMeta {
-            opcode: asm::POP,
-            mnemonic: "pop",
-            description: r#"
-                Pops 'x' elements from the stack.
-                The argument is the number of
-                elements to pop.
-                Passing zero is not an error but redundant.
-            "#,
-            stack_modificator: isize::MIN, // We don't know how many values will be popped, as it depends on the args.
-            is_modifying_flow_control: false,
-            is_modifying_stack: true,
-            is_modifying_heap: false,
-            instruction_type: InstructionType::Memory,
-            arguments: &[
-                ArgumentMeta {
-                    visibility_type: ArgumentVisibilityType::Implicit(-1),
-                    value_type: ArgumentValueType::AnyValueType,
-                    policy: ArgumentPolicy::Required,
-                    description: r#"
-                        The value/s to pop off the stack.
-                        This policy applies to all implicit parameters if
-                        there are more than one!
-                    "#,
-                },
-                ArgumentMeta {
-                    visibility_type: ArgumentVisibilityType::Explicit,
-                    value_type: ArgumentValueType::Int,
-                    policy: ArgumentPolicy::Optional(ArgumentDefaultValue::Int(1)),
-                    description: r#"
-                        Pops a specific count of values off the stack.
-                        If pop is called without any explicit parameter,
-                        the default value is 1 so it pops 1 value.
-                    "#,
-                },
-            ],
-        },
-    ];
+impl BytecodeChunk {
+    #[inline]
+    pub fn buf(&self) -> &[RecordUnion] {
+        &self.buf
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.buf.len()
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.buf.is_empty()
+    }
+
+    #[inline]
+    pub fn size(&self) -> usize {
+        self.buf.len() * std::mem::size_of::<RecordUnion>()
+    }
+
+    #[inline]
+    pub fn pc(&self) -> usize {
+        self.pc
+    }
+
+    #[inline]
+    pub fn fetch_next(&mut self) -> RecordUnion {
+        let ret = self.buf[self.pc];
+        self.pc += 1;
+        ret
+    }
+
+    #[inline]
+    pub fn jump_restart(&mut self) {
+        self.pc = 0
+    }
+
+    #[inline]
+    pub fn jump(&mut self, address: usize) {
+        self.pc = address
+    }
 }
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -204,160 +204,57 @@
 
 */
 
-use super::argument::*;
-use super::asm;
+use super::instruction::*;
+use super::record::RecordUnionDiscriminator;
+use super::stream::BytecodeStream;
+use std::{fs, io, path};
 
-/// The category of an instruction type.
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum InstructionType {
-    /// The instruction is for VM controlling.
-    System,
-
-    /// The instruction is for memory controlling.
-    Memory,
-
-    /// The instruction is for flow and brach control.
-    Control,
-
-    /// The instruction is for arithmetic operations.
-    Arithmetic,
-
-    /// This instruction is for logical operations.
-    Logical,
-
-    /// This instruction is for bitwise operations.
-    Bitwise,
+pub fn write_to_file(file: &path::Path, stream: &BytecodeStream) -> Result<(), io::Error> {
+    let mut buffer = String::with_capacity(stream.len());
+    for rec in stream.buffer() {
+        match rec.1 {
+            RecordUnionDiscriminator::Opcode => {
+                let meta = &InstructionMeta::META_TABLE[rec.0.opcode() as usize];
+                buffer.push_str(meta.mnemonic);
+            }
+            RecordUnionDiscriminator::Int => {}
+            RecordUnionDiscriminator::Float => {}
+        }
+    }
+    fs::write(file, buffer)
 }
 
-/// Represents an instruction.
-/// Contains metadata for bytecode parsing verification.
-pub struct InstructionMeta {
-    /// The opcode of this instruction taken from the list above.
-    pub opcode: u32,
-
-    /// The bytecoee mnemonic of this instruction.
-    pub mnemonic: &'static str,
-
-    /// A short but precise description of what this
-    /// instruction does and how it should be used.
-    /// If there are any dangers of using this instruction,
-    /// also write them down here!
-    pub description: &'static str,
-
-    /// Stack pointer offset after execution.
-    /// Use isize::MAX to indicate unknown push count!
-    /// Use isize::MIN to indicate unknown pop count!
-    pub stack_modificator: isize,
-
-    /// Set to true if this instruction jumps or executes any
-    /// branches or flow control modifications.
-    pub is_modifying_flow_control: bool,
-
-    /// Set to true if this instruction modifies anything on the stack.
-    pub is_modifying_stack: bool,
-
-    /// Set to true if this instruction modifies anything on the heap.
-    pub is_modifying_heap: bool,
-
-    /// The instruction type category of this instruction.
-    pub instruction_type: InstructionType,
-
-    /// Contains meta data about all arguments.
-    /// Will be heavily used by the bytecode verifier.
-    pub arguments: &'static [ArgumentMeta],
+pub mod sigils {
+    pub const TYPE: char = '%';
+    pub const IMMEDIATE: char = '&';
+    pub const SECTION: char = '@';
+    pub const PROC_DECL: char = '$';
+    pub const PROC_CALL: char = '*';
+    pub const PREPROCESSOR: char = '#';
 }
 
-impl InstructionMeta {
-    /// Contains the metadata of all instructions.
-    /// The opcodes can be used to lookup.
-    pub const META_TABLE: &'static [InstructionMeta] = &[
-        ///////////////////////////////
-        InstructionMeta {
-            opcode: asm::INTERRUPT,
-            mnemonic: "int",
-            description: r#" 
-                Interrupts execution. 
-                If 0 is passed it ends the program, else it triggers 
-                the exception with the corresponding id.
-            "#,
-            stack_modificator: 0,
-            is_modifying_flow_control: false,
-            is_modifying_stack: false,
-            is_modifying_heap: false,
-            instruction_type: InstructionType::System,
-            arguments: &[ArgumentMeta {
-                visibility_type: ArgumentVisibilityType::Explicit,
-                value_type: ArgumentValueType::Int,
-                policy: ArgumentPolicy::Required,
-                description: r#"
-                        The interrupt code. If the code is <= 0
-                        it will end the program and use it as exit code.
-                        Else an exception with the interrupt code will be triggerd.
-                        If there is not exception with that interrupt code,
-                        and unkown exeption will be raised.
-                    "#,
-            }],
-        },
-        ///////////////////////////////
-        InstructionMeta {
-            opcode: asm::PUSH,
-            mnemonic: "push",
-            description: r#"
-                Pushes the argument onto the stack.
-                The argument must be value type, no reference!
-            "#,
-            stack_modificator: 1,
-            is_modifying_flow_control: false,
-            is_modifying_stack: true,
-            is_modifying_heap: false,
-            instruction_type: InstructionType::Memory,
-            arguments: &[ArgumentMeta {
-                visibility_type: ArgumentVisibilityType::Explicit,
-                value_type: ArgumentValueType::AnyValueType,
-                policy: ArgumentPolicy::Optional(ArgumentDefaultValue::Int(0)),
-                description: r#"
-                        The value to push onto the stack.
-                        Can be any value type, which fits into a stack record.
-                    "#,
-            }],
-        },
-        ///////////////////////////////
-        InstructionMeta {
-            opcode: asm::POP,
-            mnemonic: "pop",
-            description: r#"
-                Pops 'x' elements from the stack.
-                The argument is the number of
-                elements to pop.
-                Passing zero is not an error but redundant.
-            "#,
-            stack_modificator: isize::MIN, // We don't know how many values will be popped, as it depends on the args.
-            is_modifying_flow_control: false,
-            is_modifying_stack: true,
-            is_modifying_heap: false,
-            instruction_type: InstructionType::Memory,
-            arguments: &[
-                ArgumentMeta {
-                    visibility_type: ArgumentVisibilityType::Implicit(-1),
-                    value_type: ArgumentValueType::AnyValueType,
-                    policy: ArgumentPolicy::Required,
-                    description: r#"
-                        The value/s to pop off the stack.
-                        This policy applies to all implicit parameters if
-                        there are more than one!
-                    "#,
-                },
-                ArgumentMeta {
-                    visibility_type: ArgumentVisibilityType::Explicit,
-                    value_type: ArgumentValueType::Int,
-                    policy: ArgumentPolicy::Optional(ArgumentDefaultValue::Int(1)),
-                    description: r#"
-                        Pops a specific count of values off the stack.
-                        If pop is called without any explicit parameter,
-                        the default value is 1 so it pops 1 value.
-                    "#,
-                },
-            ],
-        },
-    ];
+pub mod keywords {
+    pub const AUTO_TYPE: &str = "auto";
+    pub const SECTION: &str = "sec";
+    pub const DEFINTION: &str = "def";
+}
+
+pub mod literals {
+    pub const BIN: char = 'b';
+    pub const HEX: char = 'x';
+    pub const OCT: char = 'c';
+    pub const STR: char = '\"';
+}
+
+pub mod misc {
+    pub const ANY_TYPE: char = '_';
+    pub const ARGS_SEPERATOR: char = ',';
+    pub const IMPLICIT_ARGS: &str = "...";
+}
+
+pub mod sections {
+    pub const CONFIG: &str = "cfg";
+    pub const EXTERN: &str = "extrn";
+    pub const DATA: &str = "db";
+    pub const EXEC: &str = "exec";
 }
