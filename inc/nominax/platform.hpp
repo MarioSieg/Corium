cbe0b6c700971246750c7713b48d6a740f5fe986
msi <support@kerbogames.com>
2021-04-12 06:49:00 +0200

    system entry

--- inc/nominax/platform.hpp
+++ inc/nominax/platform.hpp
@@ -11,11 +11,9 @@
 #define NOMINAX_ARCH_ARM_32	false
 #define NOMINAX_RELEASE		false
 #define NOMINAX_DEBUG		false
-
-#define NOMINAX_32_BIT (NOMINAX_ARCH_X86_32 || NOMINAX_ARCH_ARM_32)
-#define NOMINAX_64_BIT (NOMINAX_ARCH_X86_64 || NOMINAX_ARCH_ARM_64)
-
-#define NOMINAX_POSIX (NOMINAX_OS_LINUX || NOMINAX_OS_ANDROID || NOMINAX_OS_MAC || NOMINAX_OS_IOS)
+#define NOMINAX_COM_GCC		false
+#define NOMINAX_COM_CLANG	false
+#define NOMINAX_COM_MINGW	false
 
 #if NDEBUG
 #	undef NOMINAX_RELEASE
@@ -28,14 +26,17 @@
 #if defined(_WIN32) || defined(_WIN64)
 #	undef NOMINAX_OS_WINDOWS
 #	define NOMINAX_OS_WINDOWS true
+#	define NOMINAX_OS_NAME "Windows"
 #elif defined(__APPLE__)
 #	include <TargetConditionals.h>
 #	if TARGET_OS_MAC
 #		undef NOMINAX_OS_MAC
 #		define NOMINAX_OS_MAC true
+#		define NOMINAX_OS_NAME "MacOS"
 #	elif defined(TARGET_OS_IPHONE) || defined(TARGET_IPHONE_SIMULATOR)
 #		undef NOMINAX_OS_IOS
 #		define NOMINAX_OS_IOS true
+#		define NOMINAX_OS_NAME "iOS"
 #	else
 #		error "platform.hpp: Unknown Apple OS!"
 #	endif
@@ -43,9 +44,11 @@
 #	ifdef __ANDROID__
 #		undef NOMINAX_OS_ANDROID
 #		define NOMINAX_OS_ANDROID true
+#		define NOMINAX_OS_NAME "Android"
 #	endif
 #	undef NOMINAX_OS_LINUX
 #	define NOMINAX_OS_LINUX true
+#	define NOMINAX_OS_NAME "Linux"
 #else
 #	error "platform.hpp: Unknown operating system!"
 #endif
@@ -53,15 +56,44 @@
 #if defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || defined(_M_X64) || defined(_M_AMD64)
 #	undef NOMINAX_ARCH_X86_64
 #	define NOMINAX_ARCH_X86_64 true
+#	define NOMINAX_ARCH_NAME "x86-64"
 #elif defined(i386) || defined(__i386) || defined(__i386__) || defined(_M_IX86) && !(defined(__amd64__) || defined(__amd64) || defined(__x86_64__) || defined(__x86_64) || defined(_M_X64) || defined(_M_AMD64))
 #	undef NOMINAX_ARCH_X86_32
 #	define NOMINAX_ARCH_X86_32 true
+#	define NOMINAX_ARCH_NAME "x86-32"
 #elif (defined(__arm__) || defined(_M_ARM)) && !defined(__aarch64__)
 #	undef NOMINAX_ARCH_ARM_32
 #	define NOMINAX_ARCH_ARM_32 true
+#	define NOMINAX_ARCH_NAME "ARM-32"
 #elif defined(__aarch64__)
 #	undef NOMINAX_ARCH_ARM_64
 #	define NOMINAX_ARCH_ARM_64 true
+#	define NOMINAX_ARCH_NAME "ARM-64"
 #else
 #	error "platform.hpp: Unknown architecture!"
+#endif
+
+#define NOMINAX_32_BIT (NOMINAX_ARCH_X86_32 || NOMINAX_ARCH_ARM_32)
+#define NOMINAX_64_BIT (NOMINAX_ARCH_X86_64 || NOMINAX_ARCH_ARM_64)
+
+#if NOMINAX_32_BIT
+#	define NOMINAX_ARCH_SIZE_NAME "32-Bit"
+#elif NOMINAX_64_BIT
+#	define NOMINAX_ARCH_SIZE_NAME "64-Bit"
+#endif
+
+#define NOMINAX_POSIX (NOMINAX_OS_LINUX || NOMINAX_OS_ANDROID || NOMINAX_OS_MAC || NOMINAX_OS_IOS)
+
+#ifdef __GNUC__
+#	undef NOMINAX_COM_GCC	
+#	define NOMINAX_COM_GCC true
+#	define NOMINAX_COM_NAME "GCC"
+#elif defined(__clang__)
+#	undef NOMINAX_COM_CLANG
+#	define NOMINAX_COM_CLANG true
+#	define NOMINAX_COM_NAME "Clang"
+#elif defined(__MINGW32__) || defined(__MINGW32__)
+#	undef NOMINAX_COM_MINGW
+#	define NOMINAX_COM_MINGW true
+#	define NOMINAX_COM_NAME "MinGW"
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,32 @@
-auto main() -> int {
-	return 0;
+#include <iostream>
+#include <string_view>
+#include <thread>
+
+#include "../inc/nominax/info.hpp"
+#include "../inc/nominax/platform.hpp"
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+auto main() -> int
+try {
+
+	std::ios_base::sync_with_stdio(false);
+
+	cout << nominax::logo;
+	cout << nominax::info_notice;
+
+	cout << "Nominax Version: " << nominax::system_version;
+	cout << "\nPlatform: " NOMINAX_OS_NAME " " NOMINAX_ARCH_SIZE_NAME;
+	cout << "\nArch: " << NOMINAX_ARCH_NAME;
+	cout << "\nPosix: " << std::boolalpha << NOMINAX_POSIX;
+	cout << "\nCompiler: " << NOMINAX_COM_NAME;
+	cout << "\nBooting runtime system...";
+}
+catch (const std::exception& ex) {
+	cerr << "[!] Fatal system exception: " << ex.what() << endl;
+}
+catch (...) {
+	cerr << "[!] Unknown system error!" << endl;
 }
