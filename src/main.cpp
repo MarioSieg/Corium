b81d93997be6ec8113ee53b4640af534fcf8adb1
msi <support@kerbogames.com>
2021-04-12 07:06:48 +0200

    machine class rating

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,28 @@ try {
 	cout << "\nArch: " << NOMINAX_ARCH_NAME;
 	cout << "\nPosix: " << std::boolalpha << NOMINAX_POSIX;
 	cout << "\nCompiler: " << NOMINAX_COM_NAME " - C++ 20";
-	cout << "\nBooting runtime system...";
+	
+	cout << "\n\nBooting runtime system...";
+	cout << "\nTID: 0x" << std::hex << std::this_thread::get_id() << std::dec;
+
+	const auto threads = std::thread::hardware_concurrency();
+	cout << "\nThreads: " << threads;
+	cout << "\nMachine class: ";
+
+	if (threads <= 2) [[unlikely]] {
+		cout << 'F';
+	} else if (threads <= 4) [[unlikely]] {
+		cout << 'E';
+	} else if (threads <= 8) [[likely]] {
+		cout << 'D';
+	} else if (threads <= 16) [[likely]] {
+		cout << 'C';
+	} else if (threads <= 32) [[likely]] {
+		cout << 'B';
+	} else [[likely]] {
+		cout << 'A';
+	}
+	cout << '\n';
 }
 catch (const std::exception& ex) {
 	cerr << "[!] Fatal system exception: " << ex.what() << endl;
