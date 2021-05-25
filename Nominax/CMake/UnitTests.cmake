9643430ae74d40f1ac36bb264716714192d13c46
msi <support@kerbogames.com>
2021-05-25 16:44:38 +0200

    Update UnitTests.cmake

--- Nominax/CMake/UnitTests.cmake
+++ Nominax/CMake/UnitTests.cmake
@@ -5,7 +5,7 @@ IF (${CORIUM_BUILD_UNIT_TESTS})
 	ADD_TEST(NAME "NominaxUnitTest" COMMAND "NominaxUnitTests")
 	TARGET_INCLUDE_DIRECTORIES("NominaxUnitTest" PUBLIC "${TEST_INCLUDE}")
 	TARGET_LINK_LIBRARIES("NominaxUnitTest" "gtest")
-	TARGET_LINK_LIBRARIES("NominaxUnitTest" "NominaxTest")
+	TARGET_LINK_LIBRARIES("NominaxUnitTest" "NominaxRuntimeTest")
 	ADD_COMPILE_DEFINITIONS("NOMINAX_TESTING")
 
 	# with GCC we need to link posix threads
