23928a9dd2db8c0cb64c888566197a14028e0691
Mario <support@kerbogames.com>
2021-07-04 17:51:45 +0200

    Unlinked nominax

--- Corium/CMake/Build.cmake
+++ Corium/CMake/Build.cmake
@@ -5,9 +5,6 @@ FILE(GLOB_RECURSE "CORIUM_PARSER_SOURCES" "Corium/Parser/*.cpp"  "Corium/Parser/
 ADD_LIBRARY("CoriumParser" STATIC "${CORIUM_PARSER_SOURCES}")
 TARGET_COMPILE_OPTIONS("CoriumParser" PRIVATE "-std=c++17")
 TARGET_COMPILE_OPTIONS("CoriumParser" PRIVATE "-fexceptions")
-
-TARGET_INCLUDE_DIRECTORIES("CoriumC" PRIVATE "Nominax/Include/")
-TARGET_LINK_LIBRARIES("CoriumC" "NominaxRuntime")
 TARGET_LINK_LIBRARIES("CoriumC" "CoriumParser")
 
 INCLUDE("Corium/CMake/Extern.cmake")
